f8249e3a9f1da31247f0acb1e5599a81158caa90
Ben <inbox@benmagill.co.uk>
2021-02-07 21:23:48 +0000

    [C] add logout

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Navbar} from "react-bootstrap"
 import {Link} from "react-router-dom"
+import { AuthContext } from '../../store/AuthStore'
 import "./index.css"
 
 export default function Index() {
+    const Auth = useContext(AuthContext)
+
+    const handleLogout = () => {
+        Auth.setLoggedIn(false)
+        Auth.setUserData({})
+    }
+
+
     return (
         <div>
             <Navbar bg="light" expand="lg">
-                <Link className="navbarLink" to="/files">Files</Link>
-                <Link className="navbarLink" to="/recent">Recent</Link>
+                {Auth.loggedIn? 
+                <div style={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+                    <div>
+                        <Link className="navbarLink" to="/files">Files</Link>
+                        <Link className="navbarLink" to="/recent">Recent</Link>
+                    </div>
+
+                    <div>{/* Search goes here */}</div>
+
+                    <div>
+                        <Link className="navbarLink" onClick={handleLogout}>Logout</Link>
+                    </div>
+                </div>
+                :null}
             </Navbar>
         </div>
     )
